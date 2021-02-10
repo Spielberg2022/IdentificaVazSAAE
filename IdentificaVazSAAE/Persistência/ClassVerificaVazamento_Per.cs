d43566505fc0333bcc66f6763638489d99f9a809
Wesley Silveira de Paula <wsilveiradepaula@gmail.com>
2021-02-10 19:35:02 -0300

    Atualizações grid vazamentos

--- IdentificaVazSAAE/Persistência/ClassVerificaVazamento_Per.cs
+++ IdentificaVazSAAE/Persistência/ClassVerificaVazamento_Per.cs
@@ -189,7 +189,7 @@ namespace IdentificaVazSAAE.Persistência
                 comando.Parameters["@consumoMaximo"].Value = Double.Parse(vazamento_Dom.consumoPadraoMaximo.ToString());
 				adaptador.SelectCommand = comando;
 
-				comando.CommandText = "select (substring(data_ref, 5, 2) + '/' + substring(data_ref, 1, 4)) as [Data Ref], data_leitura as [Data da Leitura], leitura_orig as Leitura, ocorrencia_orig as Ocorrencia,consumo_faturado as [Consumo Faturado], hidrometro as Hidrometro from leituras where cod_ligacao = @ligacao and data_ref >= (select SUBSTRING((select data_ult_fech from controle),1,6) + 1 - 300) and consumo_faturado > @consumoMaximo order by data_ref desc";
+				comando.CommandText = "select (substring(data_ref, 5, 2) + '/' + substring(data_ref, 1, 4)) as [Data Ref], data_leitura as [Data da Leitura], leitura_orig as Leitura, consumo_faturado as [Consumo Faturado], hidrometro as Hidrometro from leituras where cod_ligacao = @ligacao and data_ref >= (select SUBSTRING((select data_ult_fech from controle),1,6) + 1 - 300) and consumo_faturado > @consumoMaximo and ocorrencia = 0 order by data_ref desc";
 				sqlConnection.Open();
 				adaptador.SelectCommand.ExecuteNonQuery();
 				adaptador.Fill(vazamentos);
